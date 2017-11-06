3a75f6e7fcce871c82b55662d22006a78252d371
Amund Borsand <amundster@gmail.com>
2017-11-07 00:25:12 +0100

    WinForms is getting somewhere!

---
+++ MultiFindWindowsForms/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace MultiFindWindowsForms {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.FindButton = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.KeywordTextBox = new System.Windows.Forms.TextBox();
+      this.listView1 = new System.Windows.Forms.ListView();
+      this.PathList = new System.Windows.Forms.ListView();
+      this.SuspendLayout();
+      // 
+      // FindButton
+      // 
+      this.FindButton.Location = new System.Drawing.Point(209, 12);
+      this.FindButton.Name = "FindButton";
+      this.FindButton.Size = new System.Drawing.Size(75, 23);
+      this.FindButton.TabIndex = 0;
+      this.FindButton.Text = "Find";
+      this.FindButton.UseVisualStyleBackColor = true;
+      this.FindButton.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(290, 13);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Stop";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // KeywordTextBox
+      // 
+      this.KeywordTextBox.Location = new System.Drawing.Point(13, 13);
+      this.KeywordTextBox.Name = "KeywordTextBox";
+      this.KeywordTextBox.Size = new System.Drawing.Size(190, 20);
+      this.KeywordTextBox.TabIndex = 2;
+      // 
+      // listView1
+      // 
+      this.listView1.Location = new System.Drawing.Point(209, 42);
+      this.listView1.Name = "listView1";
+      this.listView1.Size = new System.Drawing.Size(529, 480);
+      this.listView1.TabIndex = 3;
+      this.listView1.UseCompatibleStateImageBehavior = false;
+      // 
+      // PathList
+      // 
+      this.PathList.Cursor = System.Windows.Forms.Cursors.Default;
+      this.PathList.Location = new System.Drawing.Point(13, 42);
+      this.PathList.Name = "PathList";
+      this.PathList.Size = new System.Drawing.Size(190, 480);
+      this.PathList.TabIndex = 4;
+      this.PathList.UseCompatibleStateImageBehavior = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(750, 534);
+      this.Controls.Add(this.PathList);
+      this.Controls.Add(this.listView1);
+      this.Controls.Add(this.KeywordTextBox);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.FindButton);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button FindButton;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TextBox KeywordTextBox;
+    private System.Windows.Forms.ListView listView1;
+    private System.Windows.Forms.ListView PathList;
+  }
+}
+
---
+++ MultiFindWindowsForms/Form1.cs
@@ -0,0 +1,187 @@
+﻿using MultiFind;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiFindWindowsForms {
+  public partial class Form1 : Form {
+    //ConcurrentQueue<Hits> hits = new ConcurrentQueue<Hits>();
+    ObservableCollection<Hits> hits = new ObservableCollection<Hits>();
+    bool run = false;
+    ConcurrentDictionary<int, string> runningTasks = new ConcurrentDictionary<int, string>();
+    List<string> searchPaths = new List<string>();
+    private System.Threading.Timer timer;
+    private Stopwatch stopwatch;
+    private int workerThreads;
+    private int competionPortThreads;
+    private int workerThreadsMax;
+    private int competionPortThreadsMax;
+    private int workerThreadsAvailable;
+    private int completionThreadsAvailable;
+    static long fileCount;
+    static long dirCount;
+
+    public Form1() {
+      InitializeComponent();
+      searchPaths.AddRange(GetLogicalDrives());
+      /*foreach (var s in searchPaths)
+        runningTasks.Add(s);*/
+    }
+
+
+
+    string[] GetLogicalDrives() {
+      string[] drives = System.IO.Directory.GetLogicalDrives();
+      return drives;
+    }
+
+
+    void RecursiveFind(string startPath, string keyword, int driveIndex) {
+
+      string[] files = null;
+      string[] directories = null;
+
+      if (!run)
+        return;
+
+      var thread = Thread.CurrentThread.ManagedThreadId;
+
+      //Dispatcher.Invoke(() => {
+      //StatusLabel.Content = startPath;
+      //runningTasks[driveIndex] = startPath;
+      //if (!runningTasks.ContainsKey(thread))
+      //runningTasks.Add(thread, new StringWithNotify());
+      runningTasks[thread] = new string(startPath.Take(70).ToArray());
+      //CollectionViewSource.GetDefaultView(PathList)?.Refresh();        
+      //});
+
+      try {
+        files = Directory.GetFileSystemEntries(startPath, keyword);
+        directories = Directory.GetDirectories(startPath, "*.*");
+      }
+      catch (Exception e) {
+        //files = new string[1];
+        //files[0] = e.Message;
+      }
+
+      if (files != null)
+        foreach (var file in files) {
+          hits.Add(new Hits(startPath, file));
+          fileCount++;
+        }
+ 
+      if (directories != null)
+        foreach (var dir in directories)
+          ThreadPool.QueueUserWorkItem(o => {
+            RecursiveFind(dir, keyword, driveIndex);
+            dirCount++;
+          });
+
+      if (directories == null || directories.Length == 0)
+        //Dispatcher.Invoke(() => {
+        //runningTasks[driveIndex] = searchPaths[driveIndex] + " done";
+        runningTasks[thread] = "";
+      //if (runningTasks.ContainsKey(thread))
+      //runningTasks.Remove(thread);
+      //});
+
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+      if (run)
+        return;
+      hits.Clear();
+      run = true;
+
+      runningTasks.Clear();
+      /*foreach (var s in searchPaths)
+        runningTasks.Add(s);*/
+      //PathList.ItemsSource = null;
+      PathList.Clear();
+      //PathList.Items = runningTasks.Values;
+      PathList.View = View.List;
+
+      string filter;
+      if (!KeywordTextBox.Text.Contains("*") ||
+          !KeywordTextBox.Text.Contains("?"))
+        filter = "*" + KeywordTextBox.Text + "*";
+      else
+        filter = KeywordTextBox.Text;
+      int i = 0;
+      foreach (var drive in searchPaths)
+        ThreadPool.QueueUserWorkItem(o => RecursiveFind(drive, filter, i++));
+
+      fileCount = dirCount = 0;
+      timer = new System.Threading.Timer(timerCallback, null, 50, 50);
+      stopwatch = new Stopwatch();
+      stopwatch.Start();
+    }
+
+    private void timerCallback(object state) {
+      ThreadPool.GetMaxThreads(out workerThreadsMax, out competionPortThreadsMax);
+      ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionThreadsAvailable);
+      /*Dispatcher.Invoke(() => {
+        Title = stopwatch.Elapsed.ToString(@"hh\:mm\:ss")
+        + " Threads: " + (workerThreadsMax - workerThreadsAvailable)
+        + " Hits: " + fileCount
+        + " Dirs: " + dirCount
+        + " DPS: " + (int)(dirCount / stopwatch.Elapsed.TotalSeconds);
+        //NotifyCollectionChanged()
+        PathList.ItemsSource = runningTasks.Values;
+        NotifyPropertyChanged("runningTasks");
+        //PathList.Items.Refresh();
+      });*/
+
+      PathList.Invoke((MethodInvoker)delegate {
+        Text = stopwatch.Elapsed.ToString(@"hh\:mm\:ss")
+        + " Threads: " + (workerThreadsMax - workerThreadsAvailable)
+        + " Hits: " + fileCount
+        + " Dirs: " + dirCount
+        + " DPS: " + (int)(dirCount / stopwatch.Elapsed.TotalSeconds);
+
+        //PathList.Clear();
+        for(int i=0; i < runningTasks.Count; i++)
+          if (PathList.Items.Count <= i)
+            PathList.Items.Add(runningTasks.ElementAt(i).Value);
+          else
+            PathList.Items[i].Text= runningTasks.ElementAt(i).Value;
+      });
+
+      if (workerThreadsMax - workerThreadsAvailable == 1) {
+        timer.Dispose();
+        run = false;
+      }
+    }
+    
+
+    private void button2_Click(object sender, EventArgs e) {
+      run = false;
+    }
+
+
+    /*private void HitsDataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+      try {
+        Hits dataRow = (Hits)HitsDataGrid.SelectedItem;
+        int index = HitsDataGrid.CurrentCell.Column.DisplayIndex;
+        Console.WriteLine(index);
+        Console.WriteLine(dataRow);
+        if (index == 0)
+          System.Diagnostics.Process.Start(dataRow.path);
+        else
+          System.Diagnostics.Process.Start(dataRow.path + '\\' + dataRow.filename);
+      }
+      catch (Exception ex) { MessageBox.Show(ex.Message); }
+    }*/
+  }
+}
